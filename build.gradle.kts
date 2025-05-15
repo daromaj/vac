7d5c63f7bc8a2a991cea501bc14899b9020f90a8
Darek <darek@homeassistant>
2025-05-15 18:58:38 +0200

    Updated build.gradle.kts to include dependency updates plugin and added version rejection logic for non-stable versions. Bumped versions in libs.versions.toml for several libraries, removed unused core-ktx dependency. Time to keep things fresh! 🚀

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    id("com.github.ben-manes.versions") version "0.52.0"
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
 }
